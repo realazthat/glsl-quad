58308529f9d5d0a2f39be2e719c47ed57d95e963
realazthat <realazthat@gmail.com>
2016-07-29 17:14:41 -0500

    Added script to publish static generated stuff to gh-pages.

---
+++ scripts/publish-static-demos.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -exv
+
+PROJECT_PATH="$PWD"
+
+
+PUBLISH_BRANCHES="master develop"
+if [ "$TRAVIS_REPO_SLUG" == "realazthat/glsl-quad" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [[ $PUBLISH_BRANCHES =~ "$TRAVIS_BRANCH" ]]; then
+
+  echo -e "Publishing generated static data...\n"
+
+  GENERATED_STUFF_PATH="$PWD/www/"
+
+  cd $HOME
+  
+  rm -rf gh-pages
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/realazthat/glsl-quad gh-pages > /dev/null
+
+  cd gh-pages
+  touch .nojekyll
+  
+  git rm -rf --ignore-unmatch "./$TRAVIS_BRANCH/www"
+  mkdir -p "./$TRAVIS_BRANCH/www"
+  cp -Rf "$PWD/www/" "./$TRAVIS_BRANCH/www/"
+  cp -Rf "$PWD/dist/" "./$TRAVIS_BRANCH/dist/"
+  git add -f .
+  git -c user.email="travis@travis-ci.org" -c user.name="travis-ci" \
+        commit -m "Latest 'generated static data' on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages/$TRAVIS_BRANCH"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Published generated static data to gh-pages.\n"
+  
+fi
